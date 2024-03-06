57046dc64f1e1a7199a669ab9d2bd6e994a11972
Ankit Gala <58542521+ankitgala11@users.noreply.github.com>
2024-03-06 12:47:33 +0530

    Time: 35 ms (50.75%) | Memory: 16.6 MB (63.31%) - LeetSync

--- 1252-break-a-palindrome/break-a-palindrome.py
+++ 1252-break-a-palindrome/break-a-palindrome.py
@@ -5,23 +5,14 @@ class Solution:
         if n == 1:
             return ""
 
-        ans=""
+     
 
-        for i in range(n):
+        for i in range(n//2):
 
             if palindrome[i] != "a":
-                if i == n-1:
-                    ans = palindrome[:i] + "a"
-                
-                else:
+          
 
-                    ans = palindrome[:i] + "a" + palindrome[i+1:]
-                break
+                return palindrome[:i] + "a" + palindrome[i+1:]
+           
 
-        if ans==ans[::-1]:ans=""
-        if not ans:
-            ans = palindrome[:-1] + chr(ord(palindrome[-1]) + 1)
-
-
-        return ans
-
+        return palindrome[:-1] + "b"
