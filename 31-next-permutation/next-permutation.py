6411ad2502957790c278e8bef1a9f62166504bba
Ankit Gala <58542521+ankitgala11@users.noreply.github.com>
2024-03-06 16:24:32 +0530

    Time: 42 ms (66.02%) | Memory: 16.4 MB (95.05%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -1,45 +1,43 @@
+from copy import deepcopy
+
+
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
+
+        # 1 2 6 9 8 7 5 4
+        # piche se jao and find pehela aisa number joh aage wale se chota ho
+        # agar nahi milla matlab array reverse mein hai toh reverse karke return
+        # agar milla toh piche se us number tak mein pehela us number se bada ele nikalo
+        # usko uske sath replace
+        # then us number se aage ki puri string ko reverse ya sort
         """
 
         def rev(nums, i, j):
 
-            while i<j:
-                nums[i] , nums[j] = nums[j] , nums[i]
-                i+=1
-                j-=1
+            while i < j:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j -= 1
 
-            
-
-
-        idx = -1
         n = len(nums)
-
-        for i in range(n-2, -1, -1):
-
-            if nums[i] < nums[i+1]:
-                idx=i
+        idx = -1
+        for i in range(n - 2, -1, -1):
+            if nums[i] < nums[i + 1]:
+                idx = i
                 break
 
         if idx == -1:
-            rev(nums, 0, n-1)
-            return nums  
+            rev(nums, 0, n - 1)
+            return
 
-
-        for i in range(n-1, idx, -1):
-            if nums[i]>nums[idx]:
-                nums[i] , nums[idx] = nums[idx] , nums[i]
+        for i in range(n - 1, idx, -1):
+            if nums[i] > nums[idx]:
+                nums[i], nums[idx] = nums[idx], nums[i]
                 break
 
-        
-        rev(nums, idx+1, n-1)
-     
+        i = idx + 1
+        j = n - 1
 
-
-        
-
-
-
-
+        rev(nums, i, j)
