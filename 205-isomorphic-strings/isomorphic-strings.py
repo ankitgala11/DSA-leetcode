d38b3d08b9a371e86aaae63566b75ca1ae0f9a2a
Ankit Gala <58542521+ankitgala11@users.noreply.github.com>
2024-05-31 17:07:03 +0530

    Time: 51 ms (12.33%) | Memory: 16.6 MB (93.23%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,22 +1,31 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        mp_s={}
-        mp_t={}
+        s2t, t2s = {}, {}
+        for i in range(len(s)):
+            if s[i] in s2t and s2t[s[i]] != t[i]:
+                return False
+            if t[i] in t2s and t2s[t[i]] != s[i]:
+                return False
+            s2t[s[i]] = t[i]
+            t2s[t[i]] = s[i]
+        return True
+        # mp_s={}
+        # mp_t={}
 
-        n=len(s)
+        # n=len(s)
 
-        for i in range(n):
+        # for i in range(n):
 
-            if s[i] in mp_s:
-                mp_s[s[i]].append(i)
-            elif s[i] not in mp_s:
-                mp_s[s[i]]= [i]
-
-
-            if t[i] in mp_t:
-                mp_t[t[i]].append(i)
-            elif t[i] not in mp_t:
-                mp_t[t[i]] = [i]
+        #     if s[i] in mp_s:
+        #         mp_s[s[i]].append(i)
+        #     elif s[i] not in mp_s:
+        #         mp_s[s[i]]= [i]
 
 
-        return list(mp_s.values()) == list(mp_t.values())
+        #     if t[i] in mp_t:
+        #         mp_t[t[i]].append(i)
+        #     elif t[i] not in mp_t:
+        #         mp_t[t[i]] = [i]
+
+
+        # return list(mp_s.values()) == list(mp_t.values())
